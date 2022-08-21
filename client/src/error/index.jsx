9f83ea792fa85b92e70e6a98b3fa7a754c873d3b
Anderson <andersonventosilla0@gmail.com>
2022-08-21 18:51:44 -0500

    spinner bootstrap done

--- client/src/error/index.jsx
+++ client/src/error/index.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Error() {
   return (
-    <div className="loadingCss">
-      <h1>Loading Countries....</h1>
+    <div className="text-center p-5">
+      <button className="btn btn-primary px-5 py-3" type="button" disabled>
+        <span
+          className="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        <h2>Loading countries...</h2>
+      </button>
     </div>
   );
 }
